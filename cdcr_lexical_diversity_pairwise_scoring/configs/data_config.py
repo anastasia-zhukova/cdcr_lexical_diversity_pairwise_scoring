7823ab1aec1d0a9c45bb7b5665cca46291b03517
Sergei Zakharov <thesdzakharov@gmail.com>
2026-02-13 22:15:43 +0300

    Refactored config handling a bit.

---
+++ cdcr_lexical_diversity_pairwise_scoring/configs/data_config.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from cdcr_lexical_diversity_pairwise_scoring.dataobjs.topics import ScopeConfig
+from cdcr_lexical_diversity_pairwise_scoring.dataobjs.ucdcr_dataset import (
+    DatasetSetting,
+    MentionPairStrategy,
+)
+
+
+@dataclass
+class DataConfig:
+    dataset_folder: Path  # Global for all run
+    setting: DatasetSetting  # Global for all run
+    ratio: int  # Global for all run
+    ##
+    train_scope: ScopeConfig
+    dev_scope: ScopeConfig
+    test_scope: ScopeConfig
+    ##
+    type_of_pairs: MentionPairStrategy
+    dev_type_of_pairs: MentionPairStrategy
+    # test_type_of_pairs is always set to MentionPairStrategy.all
+    ##
+    max_pairs_train: int
+    max_pairs_dev: int
+    # max_pairs_test is always set to None
+    ##
+    train_dataset_names: list[str]  # Shared with dev_dataset_name
+    # dev_dataset_names is the same as train_dataset_names
+    test_dataset_names: list[str]
--- cdcr_lexical_diversity_pairwise_scoring/preprocess_gen_pairs.py
+++ cdcr_lexical_diversity_pairwise_scoring/preprocess_gen_pairs.py
@@ -1,54 +1,29 @@
 import random
-from dataclasses import dataclass
-from pathlib import Path
 
 import hydra
 from hydra.core.config_store import ConfigStore
 
 from cdcr_lexical_diversity_pairwise_scoring import logger
+from cdcr_lexical_diversity_pairwise_scoring.configs import DataConfig
 from cdcr_lexical_diversity_pairwise_scoring.constants import PROJECT_ROOT
 from cdcr_lexical_diversity_pairwise_scoring.dataobjs.topics import ScopeConfig
 from cdcr_lexical_diversity_pairwise_scoring.dataobjs.ucdcr_dataset import (
     DatasetSetting,
-    MentionPairStrategy,
     Split,
     uCDCRDataSet,
 )
 
 
 # TODO Sergei: fix the configs to be pluggable for each experiment
-CONFIG_NAME = "preprocess_E4-2-4"
-
-
-@dataclass
-class PreprocessGenPairsConfig:
-    dataset_folder: Path  # Global for all run
-    setting: DatasetSetting  # Global for all run
-    ratio: int  # Global for all run
-    ##
-    train_scope: ScopeConfig
-    dev_scope: ScopeConfig
-    test_scope: ScopeConfig
-    ##
-    type_of_pairs: MentionPairStrategy
-    dev_type_of_pairs: MentionPairStrategy
-    # test_type_of_pairs is always set to MentionPairStrategy.all
-    ##
-    max_pairs_train: int
-    max_pairs_dev: int
-    # max_pairs_test is always set to None
-    ##
-    train_dataset_names: list[str]  # Shared with dev_dataset_name
-    # dev_dataset_names is the same as train_dataset_names
-    test_dataset_names: list[str]
+CONFIG_NAME = "preprocess_E1-1-1"
 
 
 cs = ConfigStore.instance()
-cs.store(name=CONFIG_NAME, node=PreprocessGenPairsConfig)
+cs.store(name=CONFIG_NAME, node=DataConfig)
 
 
-@hydra.main(version_base="1.3", config_path=str(PROJECT_ROOT / "config"), config_name=CONFIG_NAME)
-def main(config: PreprocessGenPairsConfig) -> None:
+@hydra.main(version_base="1.3", config_path=str(PROJECT_ROOT / "config/data_configs"), config_name=CONFIG_NAME)
+def main(config: DataConfig) -> None:
     logger.debug(config)
     random.seed(0)
     dataset_dict = {}
